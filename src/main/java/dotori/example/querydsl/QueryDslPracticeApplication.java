a90ea450e5fb411127700f3b7cc6b7ee34fab5b8
ChoiJoongHyun <joonghyun0117@gmail.com>
2017-12-28 14:44:18 +0900

    first commit

---
+++ src/main/java/dotori/example/querydsl/QueryDslPracticeApplication.java
@@ -0,0 +1,12 @@
+package dotori.example.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslPracticeApplication.class, args);
+	}
+}
