b59fbf149f81d3e6fd07e0b0169b9ba99ce62abb
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-03-02 14:33:00 +0900

    불필요 코드 제거.

--- src/main/java/dotori/example/querydsl/repository/UserRepositoryCustom.java
+++ src/main/java/dotori/example/querydsl/repository/UserRepositoryCustom.java
@@ -9,24 +9,7 @@ import java.util.List;
  */
 public interface UserRepositoryCustom {
     List<User> findWithArticle();
-    List<User> findWithArticle_fetchJoin();
 
     List<User> findWithArticle_streamDistinct();
     List<User> findWithArticle_selectDistinct();
-
-    User findByIdx(Long idx);
-    User findByIdxQueryFactory(Long idx);
-
-    User findWithArticleByIdxAndCategoryQueryFactory(Long idx, String category);
-    /*User findByIdx(Long idx);
-
-
-    User findWithArticleByIdx(Long idx);
-    List<User> findListWithArticleByIdx(Long idx);
-
-
-
-    List<User> findWithArticle();
-
-    List<User> findWithArticleByIdxIn(List<Long> idxList);*/
 }
--- src/main/java/dotori/example/querydsl/repository/UserRepositoryImpl.java
+++ src/main/java/dotori/example/querydsl/repository/UserRepositoryImpl.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.support.QueryDslRepositorySupport
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static dotori.example.querydsl.domain.QUser.user;
 import static dotori.example.querydsl.domain.QArticle.article;
+import static dotori.example.querydsl.domain.QUser.user;
 /**
  * Created by joonghyun on 2017. 12. 30..
  */
@@ -57,110 +57,4 @@ public class UserRepositoryImpl extends QueryDslRepositorySupport implements Use
                 .leftJoin(user.articles, article)
                 .fetch();
     }
-
-    /**
-     * user left fetch join article
-     * */
-    @Override
-    public List<User> findWithArticle_fetchJoin() {
-        return this.queryFactory.select(user)
-                .from(user)
-                .leftJoin(user.articles, article)
-                .fetchJoin()
-                .fetch();
-    }
-
-
-
-
-
-
-
-    @Override
-    public User findByIdx(Long idx) {
-        return from(user).where(user.idx.eq(idx)).fetchOne();
-    }
-
-    @Override
-    public User findByIdxQueryFactory(Long idx) {
-        return this.queryFactory.selectFrom(user)
-                .innerJoin(user.articles, article)
-                .fetchJoin()
-                .where(user.idx.eq(idx))
-                .fetchOne();
-    }
-
-    @Override
-    public User findWithArticleByIdxAndCategoryQueryFactory(Long idx, String category) {
-
-        //return from(user).select(user.idx).distinct().fetch();
-        //return this.queryFactory.select()
-        return this.queryFactory.selectFrom(user)
-                .innerJoin(user.articles, article)
-                .fetchJoin()
-                .where(user.idx.eq(idx)
-                        .and(article.category.eq(category)))
-                .fetchOne();
-    }
-
-    /*@Override
-    public User findByIdx(Long idx) {
-        return from(user)
-                .where(user.idx.eq(idx))
-                .fetchOne();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Override
-    public List<User> findListWithArticleByIdx(Long idx) {
-        return from(user)
-                .leftJoin(user.articles, article)
-                .fetchJoin()
-                .where(user.idx.eq(idx))
-                .fetch();
-    }
-
-    @Override
-    public User findWithArticleByIdx(Long idx) {
-        return from(user)
-                .leftJoin(user.articles, article)
-                .fetchJoin()
-                .where(user.idx.eq(idx))
-                .fetchOne();
-    }
-
-    @Override
-    public List<User> findWithArticle() {
-        return from(user)
-                .distinct() //중복제거를 하지 않으면 user 리스트는 9가 되는구나. api 확인해보자 (특정 user 를 검색하면 distinct 안해도된다)
-                .leftJoin(user.articles, article)
-                .fetchJoin()
-                .fetch();
-    }
-
-    @Override
-    public List<User> findWithArticleByIdxIn(List<Long> idxList) {
-        return from(user)
-                .distinct()
-                .leftJoin(user.articles, article)
-                .fetchJoin()
-                .where(user.idx.in(idxList))
-                .fetch();
-    }*/
 }
