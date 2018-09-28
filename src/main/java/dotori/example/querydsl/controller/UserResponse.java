2569d1c5eb839603c4f93fdd1a5f554e33f14ac4
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-09-28 14:50:30 +0900

    불필요 Copyright 주석 제거.

--- src/main/java/dotori/example/querydsl/controller/UserResponse.java
+++ src/main/java/dotori/example/querydsl/controller/UserResponse.java
@@ -1,16 +1,3 @@
-/* Copyright (c) 2018 ZUM Internet, Inc.
- * All right reserved.
- * http://www.zum.com
- * This software is the confidential and proprietary information of ZUM
- * , Inc. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement
- * you entered into with ZUM.
- *
- * Revision History
- * Author                    Date                     Description
- * ------------------       --------------            ------------------
- *   joonghyun                2018-01-05
- */
 package dotori.example.querydsl.controller;
 
 import lombok.Builder;
