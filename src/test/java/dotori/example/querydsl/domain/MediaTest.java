8e96d58414d92dd722c50a6d66786da836d6819f
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-11-02 19:11:31 +0900

    불필요 주석 제거.

--- src/test/java/dotori/example/querydsl/domain/MediaTest.java
+++ src/test/java/dotori/example/querydsl/domain/MediaTest.java
@@ -2,30 +2,13 @@ package dotori.example.querydsl.domain;
 
 import dotori.example.querydsl.repository.MediaRepository;
 import org.hamcrest.core.Is;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
 import static org.junit.Assert.*;
-
-/* Copyright (c) 2018 ZUM Internet, Inc.
- * All right reserved.
- * http://www.zum.com
- * This software is the confidential and proprietary information of ZUM
- * , Inc. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement
- * you entered into with ZUM.
- *
- * Revision History
- * Author                    Date                     Description
- * ------------------       --------------            ------------------
- *   joonghyun                2018-11-02
- */
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
